8dab5c3d1eba1093b3f30f4a993e875db4d6772d
sbeck2706 <sbeck@student.ethz.ch>
2018-07-23 21:44:00 +0200

    add sns message for getting feedback when function done

---
+++ notifier/handler.py
@@ -0,0 +1,38 @@
+import urllib
+import os
+import boto3
+
+def start_label_detection(bucket, key):
+  rekognition_client = boto3.client("rekognition")
+  response = rekognition_client.start_label_detection(
+    Video={
+      "S3Object":{
+        "Bucket": bucket,
+        "Name": key
+      }
+    },
+    NotificationChannel={
+      "SNSTopicArn": os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+      "RoleArn": os.environ['REKOGNITION_ROLE_ARN']
+    })
+
+  print(response)
+  return
+
+
+def start_processing_video(event, context):
+  """Lambda Function to be called """
+  for record in event["Records"]:
+    start_label_detection(
+      record["s3"]["bucket"]["name"],
+      urllib.parse.unquote_plus(record["s3"]["object"]["key"])
+    )
+
+  print(event)
+
+  return
+
+def handle_lable_detection(event, context):
+
+  print(event)
+  return
